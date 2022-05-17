59849e512d0b7d3f83bb1f9a742f20edd0ee2b63
Alexandr Drankevich <a.a.drankevich@gmail.com>
2022-05-17 19:34:06 +0300

    1

--- src/test/java/pageobjects/SearchResultPage.java
+++ src/test/java/pageobjects/SearchResultPage.java
@@ -11,6 +11,7 @@ import java.util.stream.Collectors;
 public class SearchResultPage extends AbstractPage {
 
     List<String> listResorts;
+    List<String> listHotelNames;
 
     protected SearchResultPage(WebDriver driver) {
         super(driver);
@@ -21,12 +22,16 @@ public class SearchResultPage extends AbstractPage {
     @FindBy(xpath = "//iframe[@id='tcjsfr_7bcf672ed6f2f653cc2abf8929877414']")
     private WebElement frameResult;
     private String searchResultLocator = "//div[@class='search-results-place-wrap']/a";
+    private String nameHotelLocator = "//div[@class='search-results-title']";
 
     public void getListResorts() {
         driver.switchTo().defaultContent().switchTo().frame(frameResult);
         List<WebElement> elements = waitForPresenceOfAllElementsLocatedBy(searchResultLocator);
+        List<WebElement> names = waitForPresenceOfAllElementsLocatedBy(nameHotelLocator);
+        listHotelNames = names.stream().map(s -> s.getText()).collect(Collectors.toList());
         listResorts = elements.stream().map(s -> s.getText()).collect(Collectors.toList());
         logger.info(listResorts);
+        logger.info(listHotelNames);
     }
 
     public boolean isResultsSearchContainsResort(String resortName) {
